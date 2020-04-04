affee19f12991ae5351ff12077dee319ce23e2f5
Charutha Bandara <charuthabandara@gmail.com>
2020-04-04 14:50:02 -0400

    added live editor

--- live-css-editor/src/App.js
+++ live-css-editor/src/App.js
@@ -1,17 +1,47 @@
-import React from 'react';
+import React, { Component } from "react";
 import EditBox from './components/EditBox/EditBox';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
-          Live CSS Editor
-        </h1>
-      </header>
-      <EditBox></EditBox>
-    </div >
-  );
-}
+export default class App extends Component {
 
-export default App;
+  constructor(props) {
+    super(props);
+    this.state = {
+      cssInput: ""
+    }
+  }
+
+
+  handleChange = (e) => {
+    this.setState({
+      cssInput: e.target.value
+    })
+
+  }
+
+  render() {
+    const style = this.state.cssInput;
+    const example = "* {color: red !important;}"
+    return (
+      <div className="App" >
+        <style dangerouslySetInnerHTML={{ __html: style }}></style>
+        <header className="App-header">
+          <h1>
+            Live CSS Editor
+          </h1>
+          <p>Try entering some css</p>
+          <p>Example: {example}</p>
+        </header>
+        <body>
+          <div className="edit-box">
+            <form>
+              <textarea className="css-input" id="css-input" name="css-input" onChange={this.handleChange}></textarea>
+            </form>
+            <p>{this.state.cssInput}</p>
+          </div>
+        </body>
+
+      </div >
+    );
+  }
+
+}
--- live-css-editor/src/components/EditBox/EditBox.js
+++ live-css-editor/src/components/EditBox/EditBox.js
@@ -2,16 +2,17 @@ import React, { Component } from "react";
 
 export default class EditBox extends Component {
 
+  handleChange = (e) => {
+    this.setState({
+      cssInput: e.target.value
+    })
+  }
 
   render() {
     return (
       <div>
         <p>Place your css here</p>
-        <div className="edit-box">
-          <form>
-            <input className="css-input" id="css-input" name="css-input" value="css-input" type="textarea"></input>
-          </form>
-        </div>
+
       </div>
     )
   }

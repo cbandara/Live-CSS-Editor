bff47395618ff767e280939dd1e3ffce8ab3c86f
Charutha Bandara <charuthabandara@gmail.com>
2020-04-04 14:55:01 -0400

    Updates

---
+++ precache-manifest.45b67e490b2211488524925cf39cc834.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a79ea5edcf9ab9e52660a02a4306677",
+    "url": "/Live-CSS-Editor/index.html"
+  },
+  {
+    "revision": "d9a7eb3202c42a0a0685",
+    "url": "/Live-CSS-Editor/static/css/main.40dfa3eb.chunk.css"
+  },
+  {
+    "revision": "d8626512460568b21c81",
+    "url": "/Live-CSS-Editor/static/js/2.594fc457.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Live-CSS-Editor/static/js/2.594fc457.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9a7eb3202c42a0a0685",
+    "url": "/Live-CSS-Editor/static/js/main.c1f0f400.chunk.js"
+  },
+  {
+    "revision": "a1d90298cfe653cb3096",
+    "url": "/Live-CSS-Editor/static/js/runtime-main.17780671.js"
+  }
+]);

d33de01338555ab802e69d0ce22fe27fcad10f67
Charutha Bandara <charuthabandara@gmail.com>
2020-04-04 21:08:47 -0400

    Updates

--- precache-manifest.d9b08287ceded408b41971993846f005.js
+++ precache-manifest.d9b08287ceded408b41971993846f005.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d76caf2085ef78c402284c0afc2cc2c1",
+    "revision": "0f6c3d2af1b2b301dc69cf2a2b11bcdd",
     "url": "/Live-CSS-Editor/index.html"
   },
   {
-    "revision": "159ffe3931a63af2ca9e",
-    "url": "/Live-CSS-Editor/static/css/main.40dfa3eb.chunk.css"
+    "revision": "77d167326f80ba614c16",
+    "url": "/Live-CSS-Editor/static/css/main.3dd3d4f7.chunk.css"
   },
   {
     "revision": "d8626512460568b21c81",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Live-CSS-Editor/static/js/2.594fc457.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "159ffe3931a63af2ca9e",
-    "url": "/Live-CSS-Editor/static/js/main.3311f661.chunk.js"
+    "revision": "77d167326f80ba614c16",
+    "url": "/Live-CSS-Editor/static/js/main.6b05bbe6.chunk.js"
   },
   {
     "revision": "a1d90298cfe653cb3096",
